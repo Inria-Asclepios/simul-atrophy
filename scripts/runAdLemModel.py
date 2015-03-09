8539aaa32fcb1c29ee51aa2310ab721b44cd1752
Bishesh Khanal <bisheshkh@gmail.com>
2015-03-09 15:37:20 +0100

    Combined runAdLemmodel.py for both cluster and local use. Launches job from within the script eliminating the need of a separate .pbs file.

--- scripts/runAdLemModel.py
+++ scripts/runAdLemModel.py
@@ -1,99 +1,140 @@
 #!/usr/bin/env python
-import subprocess
+import os
+import os.path as op
 import sys
 import optparse
+import bish_utils as bu
 
-workDir = "/user/bkhanal/home/works/AdLemModel/"
-target = workDir + "build/src/AdLemMain"
-petscCommand = "/home/bkhanal/Documents/softwares/petsc/bin/petscmpiexec -n 4 "
+def get_input_options():
+    """ command line interface, get input options and interact with the user
+    """
+    #parse the command line inputs
+    parser = optparse.OptionParser()
+    parser.add_option('-p', '--patientID', dest='patient', help='patient dir at'
+                      ' $ADLEM_DIR/results/patients/ which contains all the '
+                      'inputs required by this script. Results will also be '
+                      ' written here.')
+    parser.add_option('-a', '--atrophy', dest='atrophy', help='input atrophy '
+                      'file.')
+    parser.add_option('-s', '--in_seg', dest='in_seg', help='input brain seg ')
+    parser.add_option('--use_dti', dest='use_dti', action='store_true', help=
+                      'If the option is provided, input DTI must be given with'
+                      ' the option -d')
+    parser.add_option('-d', '--in_dti', dest='in_dti', help='valid '
+                      'tensor image file present in patientID directory.')
+    parser.add_option('-f', '--res_prefix', dest='res_prefix', help='prefix to'
+                      ' be added to all the output filenames')
+    parser.add_option('-i', '--in_img', dest='in_img', help='valid input image '
+                      'that will be warped by the obtained displacement fields '
+                      'from the model.')
+    parser.add_option('-t', '--time_steps', dest='time_steps', help='(integer) '
+                      'number of time steps you want to solve the system.')
+    parser.add_option('--wrt_press', dest='wrt_press', action='store_true',
+                      help='Write pressure map on the disk.')
+    parser.add_option('--wrt_force', dest='wrt_force', action='store_true',
+                      help='Write force on the the disk.')
+    parser.add_option('--wrt_residual', dest='wrt_residual', action='store_true'
+                      , help='Write residual on the disk.')
+    parser.add_option('--in_cluster', dest='in_cluster', action='store_true',
+                      help='If the option is provided, launches as a job on the'
+                      ' cluster.')
 
-#parse the command line inputs
-parser = optparse.OptionParser()
-parser.add_option('-p', '--patientID', dest='patient', help='patient ID name which is also a valid directory containing the div/atrophy map')
-parser.add_option('-a', '--atrophyFile', dest='atrophyFile',help='valid atrophy filename with extension present in patientID directory')
-parser.add_option('-m', '--maskFile', dest='maskFile', help='valid brain Mask/segmentation filename with extension present in patientID data directory')
-parser.add_option('--useTensorLambda', dest='useTensorLambda', action = 'store_true', help='If the option is provided, a valid lambdaFile must be given with the option -l')
-parser.add_option('-l', '--lambdaFile', dest='lambdaFile', help='valid tensor image filename (if useTensorLambda is set to true) with extension present in patientID directory.')
-parser.add_option('-f', '--resultsFilenamesPrefix', dest='resultsFilenamesPrefix', help='prefix to be added to all the output filenames')
-parser.add_option('-i', '--imageFile', dest='imageFile', help='valid input image that will be warped by the obtained displacement fields from the model.')
-parser.add_option('-t', '--numOfTimeSteps', dest='numOfTimeSteps', help='(integer) number of time steps you want to solve the system.')
-parser.add_option('--writePressure', dest='writePressure', action = 'store_true', help='If the option is not provided pressure map will not be written to the disk.')
-parser.add_option('--writeForce', dest='writeForce', action = 'store_true', help='If the option is not provided, force term will not be written to the disk.')
-parser.add_option('--writeResidual',dest='writeResidual', action = 'store_true', help='If the option is not provided, residual will not be written to the disk.')
-
-(options, args) = parser.parse_args()
-
-if options.patient is None:
-    options.patient = raw_input('Enter a valid existing patient directory name')
-#Data and Results Directory:
-resultsDir = workDir + "results/patients/" + options.patient + "/"
-#create if not created: Nah, should be already created to have atrophyFile and MaskFile.
-#subprocess.call("mkdir -p " + resultsDir, shell=True) 
-
-if options.numOfTimeSteps is None:
-    options.numOfTimeSteps = raw_input('Enter a number of time steps you want to solve the system.')
-
-if options.imageFile is None:
-    options.imageFile = raw_input('Enter a valid existing image file that will be warped by the obtained displacement fields')
-imageFile = resultsDir + options.imageFile
-
-if options.atrophyFile is None:
-    options.atrophyFile = raw_input('Enter a valid atrophy filename')
-atrophyFile = resultsDir + options.atrophyFile
-
-if options.maskFile is None:
-    options.maskFile = raw_input('Enter a valid mask/segmented image filename')
-maskFile = resultsDir + options.maskFile
-
-#if options.useTensorLambda is None:
-#    options.useTensorLambda = raw_input('provide true or false depending on if you intend to use a lambda from a tensor image.')
-
-if options.useTensorLambda is True:
-    useTensorLambdaString = 'true'
-    if options.lambdaFile is None:
-        options.lambdaFile = raw_input('Enter a valid tensor image filename, or simply put a random string if you are not using tensor image by internally changing the PetscAdLemMain.cxx file')
-    lambdaFile = resultsDir + options.lambdaFile
-else:
-    useTensorLambdaString = 'false'
-    lambdaFile = "dummyFileName"
-
-if options.resultsFilenamesPrefix is None:
-    options.resultsFilenamesPrefix = raw_input('Enter a prefix string that will be added to all the output filenames')
-
-if options.writePressure is True:
-    writePressureString = 'true'
-else:
-    writePressureString = 'false'
-
-if options.writeForce is True:
-    writeForceString = 'true'
-else:
-    writeForceString = 'false'
-
-if options.writeResidual is True:
-    writeResidualString = 'true'
-else:
-    writeResidualString = 'false'
-
-#Petsc options:
-petscOptions = " -pc_type fieldsplit -pc_fieldsplit_type schur -pc_fieldsplit_schur_precondition self -pc_fieldsplit_dm_splits 0 -pc_fieldsplit_0_fields 0,1,2 -pc_fieldsplit_1_fields 3 "
-#petscOptions += "-pc_fieldsplit_schur_fact_type upper "
-#petscOptions = " -pc_type fieldsplit -pc_fieldsplit_type schur -pc_fieldsplit_detect_saddle_point "
-#pc types for the fields
-petscOptions += "-fieldsplit_0_pc_type hypre "
-#petscOptions += "-fieldsplit_0_pc_type lu -fieldsplit_0_pc_factor_mat_solver_package superlu_dist "
-#monitor options
-petscOptions += "-fieldsplit_1_ksp_converged_reason -ksp_converged_reason "
-#petscOptions += "-fieldsplit_1_ksp_max_it 3 -ksp_max_it 3 "
-petscOptions += "-fieldsplit_1_ksp_monitor_true_residual -ksp_monitor_true_residual " # -log_summary " # -ksp_view "
+    (ops, args) = parser.parse_args()
+    if ops.patient is None:
+        ops.patient = raw_input('Enter a valid existing patient directory name')
+    if ops.time_steps is None:
+        ops.time_steps = raw_input('Enter num_of_time_steps to solve.')
+    if ops.in_img is None:
+        ops.in_img = raw_input('Enter image file that will be warped by the '
+                               'obtained displacement fields.')
+    if ops.atrophy is None:
+        ops.atrophy = raw_input('Enter a valid atrophy file.')
+    if ops.in_seg is None:
+        ops.in_seg = raw_input('Enter a valid segmented image ')
+    if ops.use_dti is True:
+        if ops.in_dti is None:
+            ops.in_dti = raw_input('Enter a DTI file (since you are using '
+                                   '--use_dti.):  ')
+    if ops.res_prefix is None:
+        ops.res_prefix = raw_input('Enter a prefix string to be added to all '
+                                   'the output filenames')
+    return ops
 
 
+def get_petsc_options():
+    """ Return petsc options. Change the code as required for different
+    options you want. Always puts one character space at the beginning and at
+    the end.
+    """
+    # Put one character space at the beginning and the end!
+    ops = (' -pc_type fieldsplit -pc_fieldsplit_type schur '
+           '-pc_fieldsplit_schur_precondition self -pc_fieldsplit_dm_splits 0 '
+           '-pc_fieldsplit_0_fields 0,1,2 -pc_fieldsplit_1_fields 3 '
+           '-fieldsplit_0_pc_type hypre ')
+    #monitor options
+    # ops = ops + ('-fieldsplit_0_ksp_converged_reason -fieldsplit_0_ksp_max_it'
+    #            ' 100 -ksp_converged_reason')
+    ops = ops + ('-fieldsplit_1_ksp_converged_reason -ksp_converged_reason ')
+    # ops = ops + ('-fieldsplit_1_ksp_max_it 3 -ksp_max_it 3 -ksp_rtol 1.0e-8 '
+    #              '-ksp_monitor_true_residual')
+    ops = ops + ('-fieldsplit_1_ksp_monitor_true_residual -ksp_monitor_true_'
+                 'residual -log_summary -ksp_view ')
+    return ops
 
-petscCommand += target + " -atrophyFile " + atrophyFile + " -useTensorLambda " + useTensorLambdaString + " -lambdaFile " + lambdaFile + " -maskFile " + maskFile + " -imageFile " + imageFile + " -numOfTimeSteps " + options.numOfTimeSteps + " -resPath " + resultsDir + " -resultsFilenamesPrefix " + options.resultsFilenamesPrefix + " -writePressure " + writePressureString + " -writeForce " + writeForceString + " -writeResidual " + writeResidualString + petscOptions
 
-print "given command:\n" + petscCommand + "\n"
-subprocess.call(petscCommand, shell=True)
+def main():
+    """
+    Run the model adlem for a given patient, segmentation mask, atrophy map and
+    [optional DTI]."""
+
+    work_dir = os.getenv('ADLEM_DIR')
+    if work_dir is None:
+        raise ValueError('environment variable ADLEM_DIR not set.')
+    target = op.join(work_dir, 'build/src/AdLemMain')
+    ops = get_input_options()
+    res_dir = op.join(work_dir, 'results/patients', ops.patient)
+    in_img = op.join(res_dir, ops.in_img)
+    atrophy = op.join(res_dir, ops.atrophy)
+    in_seg = op.join(res_dir, ops.in_seg)
+    res_path = res_dir+'/' #due to stupid limitation in my AdLem code
+    # where it concatenates simply the -resPath argument with the file prefix.
+    if ops.use_dti is True:
+        use_dti = 'true'
+        in_dti = op.join(res_dir, ops.in_dti)
+    else:
+        use_dti = 'false'
+        in_dti = "dummyFileName"
+    if ops.wrt_press is True:
+        wrt_press = 'true'
+    else:
+        wrt_press = 'false'
+    if ops.wrt_force is True:
+        wrt_force = 'true'
+    else:
+        wrt_force = 'false'
+    if ops.wrt_residual is True:
+        wrt_residual = 'true'
+    else:
+        wrt_residual = 'false'
+    petsc_ops = get_petsc_options()
+    cmd = target + ' -atrophyFile ' + atrophy + ' -useTensorLambda ' + use_dti+\
+          ' -lambdaFile ' + in_dti + ' -maskFile ' + in_seg + ' -imageFile ' + \
+          in_img + ' -numOfTimeSteps ' + ops.time_steps + ' -resPath '+res_path\
+          + ' -resultsFilenamesPrefix ' + ops.res_prefix + ' -writePressure ' +\
+          wrt_press + ' -writeForce ' + wrt_force + ' -writeResidual ' + \
+          wrt_residual + petsc_ops
+    if ops.in_cluster is True:
+        cluster_mpi = '/opt/openmpi-gcc/current/bin/mpiexec '
+        cmd = bu.sophia_nef_pbs_setting() + cluster_mpi + cmd
+        job_name = 'AdLemModel' + ops.patient
+        #print cmd
+        bu.qsub_job(name=job_name, queue='parlong', walltime='12:00:00',
+                    procs='nodes=3:xeon:ppn=20', mem='mem=180gb',
+                    dest_dir=res_dir, cmd=cmd)
+    else:
+        #print cmd
+        bu.print_and_execute(cmd)
 
 
-
-
+if __name__ == "__main__":
+    main()
