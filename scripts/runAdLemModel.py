17dfa81f0fda0248d1ad4c513fc4753deb6f13e0
Bishesh Khanal <bisheshkh@gmail.com>
2015-12-04 17:09:59 +0100

    runAdlemModel now takes Petsc options from a file.

--- scripts/runAdLemModel.py
+++ scripts/runAdLemModel.py
@@ -33,6 +33,7 @@ def get_input_options():
                         'given with the option -d')
     parser.add_argument('-d', '--in_dti', help='valid tensor image file present'
                         ' in patientID directory.')
+    parser.add_argument('-p', '--petsc_op_file', help='file with petsc options')
     parser.add_argument('--wrt_press', action='store_true',
                         help='Write pressure map on the disk.')
     parser.add_argument('--wrt_force', action='store_true',
@@ -41,8 +42,8 @@ def get_input_options():
                         help='Write residual on the disk.')
     parser.add_argument('--in_cluster', action='store_true',
                         help='If provided, launches as a job on the cluster.')
-    parser.add_argument('--no_petsc_summary', action='store_true',
-                        help='If provided, does not detail the petsc outputs.')
+    # parser.add_argument('--no_petsc_summary', action='store_true',
+    #                     help='If provided, does not detail the petsc outputs.')
 
     ops = parser.parse_args()
     if ops.use_dti:
@@ -52,31 +53,11 @@ def get_input_options():
     return ops
 
 
-def get_petsc_options(no_detailed_output):
-    """ Return petsc options. Change the code as required for different
-    options you want.
-    """
-    ops = ('-pc_type fieldsplit -pc_fieldsplit_type schur '
-           '-pc_fieldsplit_schur_precondition self -pc_fieldsplit_dm_splits 0 '
-           '-pc_fieldsplit_0_fields 0,1,2 -pc_fieldsplit_1_fields 3 '
-           '-fieldsplit_0_pc_type hypre')
-    #monitor options
-    # ops = ops + ('-fieldsplit_0_ksp_converged_reason -fieldsplit_0_ksp_max_it'
-    #            ' 100 -ksp_converged_reason')
-    if not no_detailed_output:
-        ops = ops+(' -fieldsplit_1_ksp_converged_reason -ksp_converged_reason')
-        # ops = ops + ('-fieldsplit_1_ksp_max_it 3 -ksp_max_it 3 -ksp_rtol 1.0e-8 '
-        #              '-ksp_monitor_true_residual')
-        ops = ops + (' -fieldsplit_1_ksp_monitor_true_residual '
-                     '-ksp_monitor_true_residual -log_summary -ksp_view')
-    return ops
-
-
 def main():
     """
     Run the model adlem for a given patient, segmentation mask, atrophy map and
     [optional DTI]."""
-
+    ops = get_input_options()
     work_dir = os.getenv('ADLEM_DIR')
     if work_dir is None:
         raise ValueError('environment variable ADLEM_DIR not set.')
@@ -106,26 +87,23 @@ def main():
         wrt_residual = 'true'
     else:
         wrt_residual = 'false'
-    petsc_ops = get_petsc_options(ops.no_petsc_summary)
     if ops.mu_file:
         mu_file = op.join(res_dir, ops.mu_file)
         mu_ops = '-parameters "%s" -muFile %s' % (ops.lame_paras, mu_file)
     else:
         mu_ops = '-parameters "%s"' % (ops.lame_paras)
-
+    with open(ops.petsc_op_file, 'r') as fil:
+        # Join all lines in the file that does not start with '#'
+        # with a whitespace separator.
+        petsc_ops = ' '.join(
+            line.strip() for line in fil if not line.startswith('#'))
     cmd = ('%s %s -atrophyFile %s -maskFile %s -imageFile %s -useTensorLambda '
            '%s -lambdaFile %s -numOfTimeSteps %s -resPath %s '
            '-resultsFilenamesPrefix %s -writePressure %s -writeForce %s '
            '-writeResidual %s %s'
            % (target, mu_ops, atrophy, in_seg, in_img, use_dti, in_dti,
               ops.time_steps, res_path, ops.res_prefix, wrt_press, wrt_force,
-              wrt_residual, petsc_ops)) # check spaces in petsc_ops.
-    # cmd = target + ' -atrophyFile ' + atrophy + ' -useTensorLambda ' + use_dti+\
-    #       ' -lambdaFile ' + in_dti + ' -maskFile ' + in_seg + ' -imageFile ' + \
-    #       in_img + ' -numOfTimeSteps ' + ops.time_steps + ' -resPath '+res_path\
-    #       + ' -resultsFilenamesPrefix ' + ops.res_prefix + ' -writePressure ' +\
-    #       wrt_press + ' -writeForce ' + wrt_force + ' -writeResidual ' + \
-    #       wrt_residual + petsc_ops
+              wrt_residual, petsc_ops))
     if ops.in_cluster is True:
         cluster_mpi = '/opt/openmpi-gcc/current/bin/mpiexec '
         cmd = bu.sophia_nef_pbs_setting() + cluster_mpi + cmd
