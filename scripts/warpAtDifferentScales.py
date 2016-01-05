9b6637c5f7a2608e48b67f10fcd4018d99d80a2e
Bishesh Khanal <bisheshkh@gmail.com>
2016-01-05 17:56:59 +0100

    Adds b-spline interp. support in warping at diff. scales
    1. Refactors the code in warpAtDifferentScales.py with better form.
    2. Adds support for choosing interpolation type.

--- scripts/warpAtDifferentScales.py
+++ scripts/warpAtDifferentScales.py
@@ -1,95 +1,92 @@
 #!/usr/bin/env python
 import subprocess
 import sys
-import optparse
+import argparse as ag
+import bish_utils as bu
 
-#ASSUMPTIONS:
-#resultsPath must already exist and should contain the velocity file: vel.mha or output_stationary_velocity_field.mha
-#patiendID should be present with the image to be warped.
+def get_input_options():
+    ''' Command line interface, get user input options.
+    '''
+    parser = ag.ArgumentParser()
+    parser.add_argument('inFile', help='Input Image to be warped')
+    parser.add_argument('velFile', help='Input vel-field')
+    parser.add_argument('outPref', help='warped output filenames prefix')
+    parser.add_argument('invertField', help='true or false: invert the field '
+                        'before warping or not')
+    parser.add_argument('numOfScales', help='Total Number of time points to '
+                        ' warp image to', type=int)
+    parser.add_argument('firstScale', help='Starting time point', type=int)
+    parser.add_argument('stepSize', help='Step size between two time points',
+                        type=float)
+    parser.add_argument(
+        'compScheme', help='Computational scheme: 0 for scaling and squaring, '
+        'is faster; 1 for forward euler, is more accurate.')
+    parser.add_argument('interp', help='Possible types: '
+                        'linear/bspline/nearestneighbor')
+    parser.add_argument('-bspline_order', help='When bspline is used')
+    parser.add_argument(
+        '--save_only_4D', action='store_true', help='save only 4D image.')
 
-#parse the command line inputs
-parser = optparse.OptionParser()
-parser.add_option('-i', '--inputFile', dest='inputFile', help='Filename of the input image to be warped')
-parser.add_option('-d', '--displacementFile', dest='displacementFile', help='Filename of the field to be used to warp the input image')
-parser.add_option('-o', '--outputFile', dest='outputFile', help='Filename of the output file')
-parser.add_option('-v', '--invertField', dest='invertField', help='true or false: invert the field before warping or not')
-parser.add_option('-n', '--number', dest='numOfScales', help='Total Number of time points to warp image to', type=int)
-parser.add_option('-f', '--firstScale', dest='firstScale', help = 'Starting time point', type=int)
-parser.add_option('-s', '--step', dest='stepSize', help = 'Step size between two time points', type=float)
-parser.add_option('-c', '--compScheme', dest='compScheme', help = 'Computational scheme: 0 for scaling and squaring, is faster; 1 for forward euler, is more accurate')
-(options, args) = parser.parse_args()
+    ops = parser.parse_args()
+    print('%s will be warped\n' % (ops.inFile,))
+    print('number of timesteps: %d\n' % (ops.numOfScales,))
+    print('first scale value: %d\n' % (ops.firstScale,))
+    print ('step size: %f\n' % (ops.stepSize,))
+    return ops
 
-if options.inputFile is None:
-    options.inputFile = raw_input('Enter the input image FileName')
+def main():
+    '''
+    Warp input image using the input velocity field at different scales. The
+    scales are integrated by considering input field as a SVF and using either
+    forward Euler or scaling and squaring.
+    '''
+    #executables
+    work_dir = "/user/bkhanal/home/works/"
+    svf_exp = work_dir + "tools/svfExponential/SVFExponential"
+    warp_img = work_dir + "AdLemModel/build/src/WarpImage"
+    combine_imgs = work_dir + "tools/combineImages/build/combineImages"
+    ext = '.nii.gz'
 
-print "your input File to be warped: " + options.inputFile
+    #inputs
+    ops = get_input_options()
+    in_img = ops.inFile
+    in_vel = ops.velFile
+    scale = ops.firstScale
+    num = 1
+    out_img_pref = ops.outPref+'WarpedScale'
+    out_4D = '%sWarped4D%sSteps%s' % (ops.outPref, ops.numOfScales, ext)
+    # Make sure that out_img_pref string is not the starting string of out_4D.
+    # Because I delete individual files with out_img_pref*ext !!
+    if ops.interp == 'bspline':
+        interp_ops = ('--interpolator %s --order %s' % (
+            ops.interp, ops.bspline_order))
+    else:
+        interp_ops = '--interpolator %s' % (ops.interp)
+    while num <= ops.numOfScales:
+        out_vel = '%svelScaled%s%s' % (ops.outPref, scale, ext)
+        out_img = '%s%s%s' % (out_img_pref, num, ext)
+        #create vel field from in_vel
+        cmd = ('%s -s %s -e %s -i %s -o %s' % (
+            svf_exp, scale, ops.compScheme, in_vel, out_vel))
+        bu.print_and_execute(cmd)
+        #create warped Image from in_img using out_vel created with svf_exp
+        cmd = ('%s --inImage %s --displacementImage %s '
+               '--outImage %s --invert %s %s' % (
+                   warp_img, in_img, out_vel, out_img, ops.invertField,
+                   interp_ops))
+        bu.print_and_execute(cmd)
+        #Delete vel field
+        bu.print_and_execute('rm ' + out_vel + '\n')
+        #Go to next file
+        num += 1
+        scale += ops.stepSize
+    #combine images: ops: numOfScales imageType imageBaseFileName out_imgFileName
+    cmd = ('%s %s %s %s %s' % (
+        combine_imgs, ops.numOfScales, ext, out_img_pref, out_4D))
+    bu.print_and_execute(cmd)
+    #delete individual warped images:
+    if ops.save_only_4D:
+        bu.print_and_execute('rm %s*%s' % (out_img_pref, ext))
 
-if options.displacementFile is None:
-    options.displacementFile = raw_input('Enter the input field FileName')
-
-if options.outputFile is None:
-    options.outputFile = raw_input('Enter the output image FileName')
-
-if options.invertField is None:
-    options.invertField = raw_input('Enter invertField option: true or false')
-    
-if options.numOfScales is None:
-    options.numOfScales = int(raw_input('Enter total number of time points'))
-
-print "number of timePoints: " + str(options.numOfScales)
-
-if options.firstScale is None:
-    options.firstScale = int(raw_input('Enter starting time point:'))
-
-print "first scale value: " + str(options.firstScale)
-
-if options.stepSize is None:
-    options.stepSize = float(raw_input('Enter step size:'))
-
-print "step size: " + str(options.stepSize)
-
-if options.compScheme is None:
-    options.compScheme = (raw_input('Enter Computational scheme: 0 for scaling and squaring, is faster; 1 for forward euler, is more accurate'))
-
-#executables
-workDir = "/user/bkhanal/home/works/"
-svfExp = workDir + "tools/svfExponential/SVFExponential"
-warpImage = workDir + "AdLemModel/build/src/WarpImage"
-combineImages = workDir + "tools/combineImages/build/combineImages"
-
-#inputs of the executables
-inputImage = options.inputFile
-inputVel = options.displacementFile
-scale = options.firstScale
-num = 1
-while num <= options.numOfScales:
-    #outputs/results of the executables
-    outputDisp = "dispScaledBy" + str(scale) + ".mha"
-    outputImage = "warpedByDisp" + str(num) + ".mha"
-
-    #create displacement field from inputVel
-    svfExpOptions = " -s " + str(scale) + " -e " + options.compScheme + " -i " + inputVel + " -o " + outputDisp
-    print svfExp + svfExpOptions + "\n"
-    subprocess.call(svfExp + svfExpOptions, shell=True)
-
-    #create warped Image from inputImage using outputDisp created with svfExp
-    warpImageOptions = " --inImage " + inputImage + " --displacementImage " + outputDisp + " --outImage " + outputImage + " --invert " + options.invertField
-    print warpImage + warpImageOptions + "\n"
-    subprocess.call(warpImage + warpImageOptions, shell=True)
-
-    #Delete displacement field
-    #print "rm " + outputDisp + "\n"
-    #subprocess.call("rm " + outputDisp, shell=True)
-
-    #Go to next file
-    num += 1
-    scale += options.stepSize
-
-#combine images: options: numOfScales imageType imageBaseFileName outputImageFileName
-combineImagesOptions = " " + str(options.numOfScales) + " " + ".mha" + " " + "warpedByDisp" + " " + options.outputFile
-print combineImages + combineImagesOptions + "\n"
-subprocess.call(combineImages + combineImagesOptions, shell = True)
-#delete inidvidual images:
-#print "rm " + resultsPath + "warpedByDisp*.mha" + "\n"
-#subprocess.call("rm " + resultsPath + "warpedByDisp*.mha", shell=True)
-
+if __name__ == "__main__":
+    main()
